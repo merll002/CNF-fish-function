f01158190b515fb2c48909eef8263bb8c03a5a58
Slapdoge <76182388+merll002@users.noreply.github.com>
2025-05-26 19:09:41 +0100

    Update fish_command_not_found.fish

--- fish_command_not_found.fish
+++ fish_command_not_found.fish
@@ -13,17 +13,15 @@ function fish_command_not_found
         case '*'
             return 1
     end
-    if test $continue -eq 1
-    end
-    set package_name (pacman -Fq "/usr/bin/$argv[1]" 2>/dev/null | head -n 1)
+    set package_name (pacman -F --machinereadable "/usr/bin/$argv[1]" "/usr/lib/$argv[1]" "/usr/lib32/$argv[1]" 2>/dev/null | awk -F"\0" "{print \$2;exit}")
     if test -z "$package_name"
-        printf "$BR Couldn't find the package containing the$B $argv[1]$BR command.\n"
-        return 1
-    end
-    printf "$BG Command$B $argv[1]$BG not found, but was found in the$BY $package_name$BG package.\n"
-    read -P " Would you like to install it?$BY [y/n] $RESET" -n 1 confirm
-    switch (string lower "$confirm")
-        case y
-            yay -S --needed "$package_name"
-    end
+                printf "$BR Couldn't find the package containing the$B $argv[1]$BR command\n"
+                return 1
+        end
+        printf "$BG Command$B $argv[1]$BG not found, but was found in the$BY $package_name$BG package.\n"
+        read -P " Would you like to install it?$BY [y/n] $RESET" -n 1 confirm
+        switch (string lower "$confirm")
+                case y
+                        sudo pacman -S --needed "$package_name"
+        end
 end
